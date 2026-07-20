b512e3f1606705cd2e0d87cf315fa330baf41d15
boomarank2-commits <boomarank2@gmail.com>
2026-07-20 22:54:09 +0200

    Expose pipeline-final progress API

---
+++ src/ethusdc_bot/protocol_v3/pipeline_final_progress_api.py
@@ -0,0 +1,3 @@
+"""Stable public Task-31 result-blind progress facade."""
+from ethusdc_bot.protocol_v3.pipeline_final_progress import *  # noqa: F401,F403
+from ethusdc_bot.protocol_v3.pipeline_final_progress import __all__
